f0981e406764dbb6d209b1186ec2aaec686edda1
Ibragim <utisey@gmial.com>
2020-01-04 00:43:32 +0300

    added portfolio block

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -14,15 +14,45 @@ $(document).ready(function () {
 
 
 
-//-----------------MOBILE NAVIGATION BUTTON and MOBILE NAVIGATION----------------
+// //-----------------MOBILE NAVIGATION BUTTON and MOBILE NAVIGATION----------------
 
-const menuToggle = document.querySelector('#menu-toggle');
-const mobileNavToggle = document.querySelector('#mobile-nav');
+// const menuToggle = document.querySelector('#menu-toggle');
+// const mobileNavToggle = document.querySelector('#mobile-nav');
 
-menuToggle.onclick = function() {
-    menuToggle.classList.toggle('menu-icon-active');
-    mobileNavToggle.classList.toggle('mobile-nav--active');
+// menuToggle.onclick = function() {
+//     menuToggle.classList.toggle('menu-icon-active');
+//     mobileNavToggle.classList.toggle('mobile-nav--active');
+// };
+
+
+
+// ----------------------------------------------- portfolio__tabs----start
+
+let tab = function () {
+    let tabNav = document.querySelectorAll('.tabs-nav__item'),
+        tabContent = document.querySelectorAll('.portfolio__tabs-content'),
+        tabName;
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabNav.forEach( item => {
+            item.classList.remove( 'is-active');
+        });
+        this.classList.add('is-active');
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+        })
+    }
 };
 
+tab();
 
-
+// ----------------------------------------------- portfolio__tabs----end
